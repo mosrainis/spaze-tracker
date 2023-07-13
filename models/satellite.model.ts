d12270c7fcb6b143eac4e8c7d488062548c7e855
Mostafa Es <mostafaes5520@gmail.com>
2023-07-13 23:00:16 +0330

    add satellite data models

---
+++ models/satellite.model.ts
@@ -0,0 +1,18 @@
+export interface TleData {
+    line1: string;
+    line2: string
+}
+
+export interface CoordOptions {
+    date?: Date,
+    sighting?: boolean
+}
+
+export interface UTCDate {
+    year: number;
+    month: number;
+    date: number;
+    hour: number;
+    minute: number;
+    second: number;
+  }
