376945c336caaeb5e74b3aa50ded56f47f3c99e2
Mostafa Es <mostafaes5520@gmail.com>
2023-07-17 23:21:50 +0330

    add marker

---
+++ components/mapMarker.tsx
@@ -0,0 +1,14 @@
+import {Icon, Point} from 'leaflet';
+import marker from '@/app/icons/iss-icon.png';
+const trackerMarker = new Icon({
+    iconUrl: marker.src,
+    iconRetinaUrl: marker.src,
+    iconAnchor: undefined,
+    popupAnchor: undefined,
+    shadowUrl: undefined,
+    shadowSize: undefined,
+    shadowAnchor: undefined,
+    iconSize: new Point(50, 50)
+});
+
+export { trackerMarker };
