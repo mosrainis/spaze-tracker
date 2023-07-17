671cdb58a9e7e055c391711da637c14283fd15f8
Mostafa Es <mostafaes5520@gmail.com>
2023-07-17 23:22:22 +0330

    add map token

--- constants/trackerConstants.ts
+++ constants/trackerConstants.ts
@@ -1,5 +1,6 @@
 const TRACKER_CONSTSNTS = {
     ISS_TLE: 'https://tle.ivanstanojevic.me/api/tle/25544',
+    MAP_TOKEN: 'abb7fe49724c4335a4db5cdb14c07d4e'
   };
   
 export default TRACKER_CONSTSNTS;
