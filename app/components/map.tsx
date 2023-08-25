c738029e733495fca68b473aa772f2b98334f191
Mostafa Es <mostafaes5520@gmail.com>
2023-08-25 22:25:20 +0330

    add grundtrack logic into map compo

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -6,18 +6,34 @@ import TRACKER_CONSTSNTS from "../../constants/trackerConstants";
 import 'leaflet/dist/leaflet.css';
 import { trackerMarker } from "./mapElements";
 import { SatelliteCoord } from "../../models/satellite.model";
+import { getCoordination } from '../../helpers/satelliteRec';
+import { SatRec } from 'satellite.js';
+import { drawOrbit } from '../../helpers/mapCalc';
+import { useEffect, useState } from 'react';
 
 const MAP_URL = `https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png?apikey=${TRACKER_CONSTSNTS.MAP_TOKEN}`;
 
 interface MapInput {
-    target: SatelliteCoord;
-    incomingGroundTracks: any;
-    pastGroundTracks: any;
+    satRec: SatRec;
 }
 
-export default function Map({target, incomingGroundTracks, pastGroundTracks}: MapInput) {
+export default function Map({satRec}: MapInput) {
+    const [target, setTarget] = useState<SatelliteCoord>(getCoordination(satRec));
+
     const position: [number, number] = [ target.lat, target.long ];
+
+    const pastGroundTracks = drawOrbit({satRec, startTimeMS: new Date().getTime(), incoming: false});
+    const incomingGroundTracks = drawOrbit({satRec, startTimeMS: new Date().getTime(), incoming: true});
     
+    const setCoord = () => {
+        setTarget(getCoordination(satRec));
+    };
+
+    useEffect(() => {
+      const interval = setInterval(setCoord, TRACKER_CONSTSNTS.LIVE_REFRESH_MS);
+      return () => clearInterval(interval);
+    }, [])
+
     return (
         <div className={styles.mapContainer}>
             <MapContainer className={styles.mapElement} center={position} zoom={3} scrollWheelZoom={true}>
--- app/tracker/page.tsx
+++ app/tracker/page.tsx
@@ -1,5 +1,5 @@
-import { TleData, SatelliteCoord } from "../../models/satellite.model";
-import { getCoordination, getSatrec } from "../../helpers/satelliteRec";
+import { TleData } from "../../models/satellite.model";
+import { getSatrec } from "../../helpers/satelliteRec";
 import TRACKER_CONSTSNTS from "../../constants/trackerConstants";
 import dynamic from 'next/dynamic'
 import { drawOrbit } from "../../helpers/mapCalc";
@@ -12,44 +12,23 @@ const Map = dynamic(
 )
 
 async function getTleData(): Promise<TleData> {
-    const { ISS_TLE } = TRACKER_CONSTSNTS;
-    const url = (ISS_TLE);
-    const res = await fetch(url);
-    if (!res.ok) {
-      throw new Error('Failed to fetch data')
-    }
-    return res.json();
+  const { ISS_TLE } = TRACKER_CONSTSNTS;
+  const url = (ISS_TLE);
+  const res = await fetch(url, {cache: "force-cache"});
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json();
 }
 
 export default async function Tracker() {
   const tleData = await getTleData();
   const satRec = getSatrec(tleData);
-  const pastGroundTracks = drawOrbit({satRec, startTimeMS: new Date().getTime(), incoming: false});
-  const incomingGroundTracks = drawOrbit({satRec, startTimeMS: new Date().getTime(), incoming: true});
-
-
-  // const [satelliteCoord, seCoordinate] = useState<SatelliteCoord | undefined>(undefined);
-
-
-  // const setCoord = () => {
-  //   seCoordinate(getCoordination(satRec));
-  // };
-
-  // useEffect(() => {
-  //     setCoord();
-
-  //     const interval = setInterval(setCoord, TRACKER_CONSTSNTS.LIVE_REFRESH_MS);
-  //     return () => clearInterval(interval);
-  // }, [])
-
-  
   
   return (
     <Space direction="vertical" style={{ width: '100%' }} size={[0, 48]}>
       <Map 
-        target={getCoordination(satRec)}
-        incomingGroundTracks={incomingGroundTracks}
-        pastGroundTracks={pastGroundTracks}
+        satRec={satRec}
       />
     </Space>
   );
